7d5dad111bb60b4f7b280c931bec561705239ff8
madhavimediboina <madhavimediboina09@gmail.com>
2026-06-11 20:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function addBook(){
+function addBook() {
 
-let book=document.getElementById("bookName").value;
+let book = document.getElementById("bookName").value;
 
 if(book==""){
 alert("Enter Book Name");
 return;
 }
 
-let li=document.createElement("li");
-li.innerText=book;
+let li = document.createElement("li");
+
+li.innerHTML =
+book +
+" <button onclick='editBook(this)'>Edit</button>" +
+" <button onclick='deleteBook(this)'>Delete</button>";
 
 document.getElementById("bookList").appendChild(li);
 
 document.getElementById("bookName").value="";
+}
+
+function deleteBook(btn){
+btn.parentElement.remove();
+}
+
+function editBook(btn){
+let newBook = prompt("Enter new book name");
+
+if(newBook){
+btn.parentElement.childNodes[0].textContent = newBook + " ";
+}
 }
